0fbeaa6d41e537550c9f8388884f37db838d20f7
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-01-31 19:27:49 -0500

    sqrtH static helper method created!

--- recursion.java
+++ recursion.java
@@ -4,12 +4,16 @@ public static double sqrt(double n, double tolerance){
 
 }
 
+public static double sqrtH(double n, double guess){
+  
+}
+
 public static int fib(int n){
 
 }
 
 public static ArrayList<Integer> makeAllSums(){
-  
+
 }
 
 }
