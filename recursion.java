ad50f276a84732b0b9a1839e9025914d89ea6c57
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-01-31 19:37:58 -0500

    sqrt and sqrtH method completed!

--- recursion.java
+++ recursion.java
@@ -7,11 +7,14 @@ public class recursion{
       */
 public static double sqrt(double n, double tolerance){
   if(n == 0.0){return n;}
+  return sqrtH(n,1.0,tolerance);
 }
 
 public static double sqrtH(double n, double guess, double tolerance){
   double num = guess * guess;
-
+  if(Math.abs((n/num) - 1) < tolerance){return guess;}
+  guess = ((n/guess) + guess) / 2.0;
+  return sqrtH(n,guess,tolerance);
 }
 
 public static int fib(int n){
