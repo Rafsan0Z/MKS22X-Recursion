f134670f8b6f4d3cc238b923d38bdb643f03b529
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-01-31 19:31:01 -0500

    comments added to sqrt method!

--- recursion.java
+++ recursion.java
@@ -1,11 +1,16 @@
 public class recursion{
 
+  /*Recursively find the sqrt using Newton's approximation
+       *tolerance is the allowed percent error the squared answer is away from n.
+       *precondition: n is non-negative
+
+      */
 public static double sqrt(double n, double tolerance){
 
 }
 
 public static double sqrtH(double n, double guess){
-  
+
 }
 
 public static int fib(int n){
