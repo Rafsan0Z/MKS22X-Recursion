3232f995c1cd77e38adc59d386c06d9b45dc7ab3
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-02-25 19:44:59 -0500

    runProcess and printLines method imported from JournalDev into Recursion file!

--- recursion.java
+++ recursion.java
@@ -53,6 +53,23 @@ public static ArrayList<Integer> makeAllSumsH(int nums, ArrayList<Integer> data)
   return makeAllSumsH(nums-1,data);
 }
 
+private static void printLines(String cmd, InputStream ins) throws Exception {
+      String line = null;
+      BufferedReader in = new BufferedReader(
+          new InputStreamReader(ins));
+      while ((line = in.readLine()) != null) {
+          System.out.println(cmd + " " + line);
+      }
+    }
+
+private static void runProcess(String command) throws Exception {
+      Process pro = Runtime.getRuntime().exec(command);
+      printLines(command + " stdout:", pro.getInputStream());
+      printLines(command + " stderr:", pro.getErrorStream());
+      pro.waitFor();
+      System.out.println(command + " exitValue() " + pro.exitValue());
+    }
+
 public static void main(String[] args){
   Scanner in = new Scanner(System.in);
   System.out.println("If you wish to test using another file then press &");
